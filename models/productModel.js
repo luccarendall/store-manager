11cb74a95efe050678747eca7ca256e424cc2853
Lucca Rendall <luccarendall2@gmail.com>
2022-08-23 01:03:26 -0300

    Função de buscar produto por ID

--- models/productModel.js
+++ models/productModel.js
@@ -8,6 +8,16 @@ const getAll = async () => {
   return products;
 };
 
+// getProductById para receber os dados específicos de um produto mediante seu ID https://bit.ly/3CGljRp
+const getProductById = async (ID) => {
+  // tabnine.com/code/javascript/functions/mysql/Connection/execute
+  const query = 'SELECT * FROM products WHERE id = ?;';
+  const [productsData] = await connection.execute(query, [ID]);
+  if (productsData.length === 0) return console.log('productsData vazio');
+  return productsData;
+};
+
 module.exports = {
   getAll,
+  getProductById,
 };

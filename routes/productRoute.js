b413387861dc1399d96daf7673310c3e40713e06
Lucca Rendall <luccarendall2@gmail.com>
2022-08-24 14:49:48 -0300

    Feat: errorMiddleware

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const productController = require('../controllers/productController');
+const errorMiddleware = require('../middlewares/errorMiddleware');
+
+const app = express();
 
 // expressjs.com/pt-br/guide/using-middleware.html
 const productRoute = express.Router();
@@ -9,6 +12,8 @@ productRoute.get('/', productController.getAll);
 
 // Buscando pelo ID (bit.ly/3CGljRp)
 productRoute.get('/:id', productController.getProductById);
+
+app.use(errorMiddleware);
 
 module.exports = {
   productRoute,

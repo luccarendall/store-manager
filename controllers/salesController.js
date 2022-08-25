30a24e2af7965930f4787be0910d4d7aa2574dbe
Lucca Rendall <luccarendall2@gmail.com>
2022-08-24 23:48:52 -0300

    getSalesById -c

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -5,6 +5,14 @@ const getSalesAll = async (_req, res) => {
   return res.status(200).json(allSalesData);
 };
 
+const getSalesById = async (req, res) => {
+  const { id } = req.params;
+  const saleID = await salesService.getSalesById(id);
+  if (!saleID || saleID === undefined) return res.status(404).json({ message: 'Sale not found' });
+
+  return res.status(200).json(saleID);
+};
+
 module.exports = {
   getSalesAll,
   getSalesById,

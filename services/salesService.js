b17ea3ec604ae066c4778a6d234bee5cd0c1641d
Lucca Rendall <luccarendall2@gmail.com>
2022-08-24 23:37:18 -0300

    getSalesAll -s

---
+++ services/salesService.js
@@ -0,0 +1,10 @@
+const salesModel = require('../models/salesModel');
+
+const getSalesAll = async () => {
+  const allSalesData = await salesModel.getSalesAll();
+  return allSalesData;
+};
+
+module.exports = {
+  getSalesAll,
+};
